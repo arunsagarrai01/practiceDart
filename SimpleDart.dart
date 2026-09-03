993109f013ba59236716576d817f30e195619939
arunsagarrai01 <arunsagarrai6@gmail.com>
2026-09-03 20:18:37 +0545

    Print sum of two numbers

--- SimpleDart.dart
+++ SimpleDart.dart
@@ -1,3 +1,17 @@
+/*
+// Print your name
+
 void main() {
   print("Arun");
+}
+*/
+
+// Sum of two numbers
+void main() {
+  int a = 10;
+  int b = 20;
+
+  int sum = a + b;
+
+  print("Sum = $sum");
 }
